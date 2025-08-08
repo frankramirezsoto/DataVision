8b11e0870ae094cb260d2dd3786c313a1ee3c07b
frankramirezsoto <f.ramirez.soto@outlook.com>
2025-08-07 19:31:12 -0600

    changes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+using DataVision.Data;
+using DataVision.Services;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +13,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Database Configuration
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Services
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<ILogService, LogService>();
 
 var app = builder.Build();
 
